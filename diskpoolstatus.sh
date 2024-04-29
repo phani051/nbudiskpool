4c5e580b385f995fd6773a6b6f36f407c3707bac
Phaneendra Kumar <phani051@gmail.com>
2024-04-30 00:56:38 +0530

    Added few morwe columns and multiple master servers. Changed delimiter to "," to tabulate and made neccessary changes in awk array.

--- diskpoolstatus.sh
+++ diskpoolstatus.sh
@@ -1,23 +1,25 @@
-
-# Gets table output with Master, DP name and %full 
 /usr/openv/netbackup/local/scripts/dpstatus >~/dpout
-
-# Get's DP status from remote master and append to the dump file
-ssh -o StrictHostKeyChecking=no olph437.bodc.att.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
-
-#Move header to different file
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no olph437.bodc.att.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no tlph101.dadc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no clph444.sldc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no clph657.sldc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no tlph102.dadc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no tsprd540.edc.cingular.net ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no clph587.sldc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no tsprd653.dadc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no csprd246.sldc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no csprd370.sldc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no fsprd448.ffdc.sbc.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no alph332.aldc.att.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+ssh -t -t -i ~/pr0488 -o StrictHostKeyChecking=no asprd559.aldc.att.com ""/usr/openv/netbackup/local/scripts/dpstatus"" >>~/dpout
+sed -i 's/\(No disk volumes found!\)//' ~/dpout
+sed -i '/^\s*$/d' ~/dpout
 cat /home/pr0488/dpout | head -n 1 >~/dpstatus
-
-#rest of the entries are sorted as per the DP %full
 cat /home/pr0488/dpout | sed -n '1!p' | sort -n -r -k 3 >>~/dpstatus
-
-#Below command will be dumping data to html file in required format
 cat ~/dpstatus | {
   cat
   echo
-} | /home/pr0488/tabulatev2.sh -d " " -t "NBU DP Status" -h "Disk Pool Status" >~/dpstatus.html
-
-#End part of the html file which is responsible for coloring as per %full
+} | /home/pr0488/tabulatev2.sh -d "," -t "NBU DP Status" -h "Disk Pool Status" >~/dpstatus.html
 echo '<script>
       $("td:nth-child(3)").each(function () {
         if (parseInt($(this).text()) >= 85) {
@@ -28,11 +30,8 @@ echo '<script>
           $(this).addClass("add_green");
         }
       });
-    </script></html>' >>~/dpstatus.html
+</script></html>' >>~/dpstatus.html
 
-#Wait for the above process to complete
 sleep 30s
-
-# Mail to the team
-echo "Kindly refer the attachment for the Diskpool status(PureDiska and StoreOnce) from flpd633 and olph437 taken at $(date)" | mail -r "p.reddy8@dxc.com" -s "NBU Diskpool status" -a /home/pr0488/dpstatus.html -c a.komuravelli@dxc.com,srujana.kommu@dxc.com,p.reddy8@dxc.com swaroop.prakash@dxc.com
-#echo "Kindly refer the attachment for the Diskpool status(PureDiska and StoreOnce) from flpd633 and olph437 taken at $(date)" | mail -r "p.reddy8@dxc.com" -s "NBU Diskpool status" -a /home/pr0488/dpstatus.html   p.reddy8@dxc.com
+echo "Kindly refer the attachment for the Diskpool status(PureDisk and StoreOnce) from NetBackup taken at $(date)" | mail -r "p.reddy8@dxc.com" -s "NBU Diskpool status" -a /home/pr0488/dpstatus.html -c p.reddy8@dxc.com,swaroop.prakash@dxc.com ATT_DXC_BUILD-EBR@dxc.com
+#echo "Kindly refer the attachment for the Diskpool status(PureDiska and StoreOnce) from NetBackup taken at $(date)" | mail -r "p.reddy8@dxc.com" -s "NBU Diskpool status" -a /home/pr0488/dpstatus.html   p.reddy8@dxc.com
--- dpstatus.sh
+++ dpstatus.sh
@@ -1,33 +1,30 @@
 touch /home/pr0488/sts
 rm -f /home/pr0488/sts
-
-
 #sudo /usr/openv/netbackup/bin/admincmd/nbdevquery -liststs | awk '{print $3}' | sort | uniq > /home/pr0488/sts
-#Get only PureDisk and StoreOnce
 echo hp-StoreOnceCatalyst >/home/pr0488/sts
 echo PureDisk >>/home/pr0488/sts
 touch /home/pr0488/diskpool
 rm -f /home/pr0488/diskpool
 
-#Dump details of PureDisk and StoreOnce disk pools
 while read p; do
   sudo /usr/openv/netbackup/bin/admincmd/nbdevquery -listdv -stype $p >>/home/pr0488/diskpool
 done </home/pr0488/sts
 
-# Create a awk array with Master name, DP name and %Full
-
 awk -v hostname="$(hostname)" '
         {
             DISKPOOL=$2
-			TOTALSPAC=$6
-			AVAILPACE=$7
-			USEDSPAC=($6-$7)*100
-			DISK_LIST[DISKPOOL]+=USEDSPAC/TOTALSPAC
-			MASTER_LIST[DISKPOOL]+=MASTER
+                        TOTALSPAC=$6
+                        AVAILPACE=$7
+                        USEDSPAC=($6-$7)*100
+                        USED=($6-$7)
+                        DISK_LIST[DISKPOOL]+=USEDSPAC/TOTALSPAC
+                        MASTER_LIST[DISKPOOL]+=MASTER
+                                                TOTAL_SPACE[DISKPOOL] += (TOTALSPAC/1024)
+                                                USED_SPACE[DISKPOOL] += (USED/1024)
+                                                AVAIL_SPACE[DISKPOOL] += (AVAILPACE/1024)
         }
         END {
-			printf ("%20s%40s%27s\n", "Master","DiskPool", "Full" )
-       		for (DSK in DISK_LIST)
-                        printf ("%20s%40s%27.2f\n", hostname ,DSK, DISK_LIST[DSK] )
-       		
+                printf ("%20s%40s%27s%20s%20s%20s\n", "Master,","DiskPool,", "%Full,", "Total Capacity(TB),", "Used Capacity(TB),", "Available Capacity(TB)" )
+                for (DSK in DISK_LIST)
+                        printf ("%20s%0s%40s%0s%27.2f%0s%20.2f%0s%20.2f%0s%20.2f\n", hostname,"," ,DSK, ",", DISK_LIST[DSK],",", TOTAL_SPACE[DSK],",", USED_SPACE[DSK],",", AVAIL_SPACE[DSK])
         }' </home/pr0488/diskpool
